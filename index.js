cf09a2cbf217caddf2a3064a722111f9ed32804d
Ayush porwal <95545423+ayushporwal7078@users.noreply.github.com>
2022-10-20 09:25:41 +0530

    Add files via upload
    this is a  simple calender created using vanilla JS.

---
+++ index.js
@@ -0,0 +1,30 @@
+const monthE1  = document.querySelector(".date h1");
+const FullDateE1  = document.querySelector(".date p");
+const daysE1  = document.querySelector(".days");
+const monthInx = new Date().getMonth();
+
+const lastDay = new Date(new Date().getFullYear(), monthInx+1, 0).getDate();
+const firstDay = new Date(new Date().getFullYear(), monthInx, 1).getDay() - 1 ;
+
+const months =[
+    "January","februruy","March", "April", "May", "June"
+    ,"July","Auguast","September", "Octobor", "November", "Decmember"
+];
+
+monthE1.innerText = months[monthInx];
+FullDateE1.innerText = new Date().toDateString();
+let days = "";
+
+for(let i=firstDay;i>0;i--){
+    days += `<div class="empty"></div>`
+}
+for(let i=1; i<=lastDay ; i++){
+    if(i === new Date().getDate()){
+        days += `<div class="today">${i}</div>`;
+    }
+    else{
+        days+=`<div>${i}</div>`;
+    }
+}
+
+daysE1.innerHTML = days;
